6faa8a0f1c487fbb4c4daa2cd51a8064a9d1d1ad
martin <martin@within.co.kr>
2018-08-31 09:16:17 +0900

    Add Classnames library and apply sample code

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,16 +1,23 @@
 import React from 'react';
+import Classnames from 'classnames';
 import { observer, inject } from 'mobx-react';
 
 @inject('TodoStore')
 @observer
 export default class Todo extends React.Component {
+    toggle = () => {
+        console.log(123);
+    };
     render () {
         const {
             text,
+            isDone
         } = this.props;
         return (
-            <li className="todo-item">
-                <button className="toggle"/>
+            <li className={Classnames("todo-item", {
+                completed: isDone
+            })}>
+                <button className="toggle" onClick={this.toggle}/>
                 <div className="todo-item__view">
                     <div className="todo-item__view__text">{text}</div>
                     <button className="todo-item__destroy"/>
