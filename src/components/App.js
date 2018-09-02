008d3ffb70b072c512064498ce5b7cb156626a2e
martinyounghoonkim <hoons0131@gmail.com>
2018-09-02 12:40:40 +0900

    Add deployd library and appling todo application with this library

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,16 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Header from './Header';
 import Footer from './Footer';
 import TodoList from './TodoList';
+import { inject, observer } from "mobx-react/index";
 
+@inject('TodoStore')
+@observer
 class App extends Component {
+    componentWillMount () {
+        this.props.TodoStore.fetchTodos()
+    }
     render() {
         return (
             <div className="todo-app">
